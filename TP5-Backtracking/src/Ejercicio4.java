62897d1b32773ededac6092c7f2ff34b970ed94d
Iván Almestro <ivanalmestro@hotmail.com>
2025-05-26 00:54:06 -0300

    acomodando parametro

--- TP5-Backtracking/src/Ejercicio4.java
+++ TP5-Backtracking/src/Ejercicio4.java
@@ -5,7 +5,7 @@ Partición de conjunto. Dado un conjunto de n enteros se desea encontrar, si exi
 dos subconjuntos disjuntos, tal que la suma de sus elementos sea la misma.
 */
 public class Ejercicio4 {
-    private ArrayList<Integer> conjunto;
+    private final ArrayList<Integer> conjunto;
 
     public Ejercicio4() {
         this.conjunto = new ArrayList<>();
@@ -16,7 +16,7 @@ public class Ejercicio4 {
         ArrayList<Integer> auxB = new ArrayList<>();
 
         // Iniciamos la búsqueda desde el primer elemento
-        Solucion s = backParticionRec(conjunto, auxA, auxB, 0, 0, 0, null);
+        Solucion s = backParticionRec(conjunto, auxA, auxB, 0, 0, 0);
 
         if (s != null) {
             s.mostrarSolucion();
@@ -29,7 +29,7 @@ public class Ejercicio4 {
      * Algoritmo de backtracking para generar todas las particiones posibles en dos subconjuntos
      * y devolver la primera con sumas iguales. No explora más una vez encontrada una solución válida.
      */
-    private Solucion backParticionRec(ArrayList<Integer> conjunto, ArrayList<Integer> auxA, ArrayList<Integer> auxB, int suma1, int suma2, int i, Solucion s) {
+    private Solucion backParticionRec(ArrayList<Integer> conjunto, ArrayList<Integer> auxA, ArrayList<Integer> auxB, int suma1, int suma2, int i) {
         if (i == conjunto.size()) {
             // Caso base: ya se procesaron todos los elementos
             if (suma1 == suma2) {
@@ -43,13 +43,13 @@ public class Ejercicio4 {
 
         // Intentar agregar el elemento al subconjunto A
         auxA.add(j);
-        Solucion s1 = backParticionRec(conjunto, auxA, auxB, suma1 + j, suma2, i + 1, s);
+        Solucion s1 = backParticionRec(conjunto, auxA, auxB, suma1 + j, suma2, i + 1);
         if (s1 != null) return s1;  // Si se encontró solución, cortamos
         auxA.remove(auxA.size() - 1);  // Backtrack
 
         // Intentar agregar el elemento al subconjunto B
         auxB.add(j);
-        Solucion s2 = backParticionRec(conjunto, auxA, auxB, suma1, suma2 + j, i + 1, s);
+        Solucion s2 = backParticionRec(conjunto, auxA, auxB, suma1, suma2 + j, i + 1);
         if (s2 != null) return s2;
         auxB.remove(auxB.size() - 1);  // Backtrack
 
